8fab2fd8fefc336fe52533decd9bc6d18fd7da4c
Jacob Morris <trauma@d-140-142-21-75.dhcp4.washington.edu>
2009-12-22 14:28:08 -0800

    * Added and expanded the player model to include full contact information, start/end dates, etc.  * Established scafolding for Position model

--- player/models.py
+++ player/models.py
@@ -10,6 +10,25 @@ class Player(models.Model):
     currency = models.IntegerField()
 # TODO: make status another model so statuses can be added/changed
     status = models.CharField(max_length=25)
+    start_date = models.DateField()
+    end_date = models.DateField(blank=True)
+    grad_date = models.DateField()
+# Phone numbers
+    phone = models.CharField(max_length=10)
+    alt_phone = models.CharField(max_length=10)
+# Chat accounts
+    uw_gmail = models.EmailField(max_length=75, blank=True)
+    uw_windows_live = models.EmailField(max_length=75, blank=True)
+    gmail = models.EmailField(max_length=75, blank=True)
+    windows_live = models.EmailField(max_length=75, blank=True)
+    aim_iChat = models.CharField(max_length=75, blank=True)
+    irc_nick = models.CharField(max_length=75, blank=True)
+    irc_network = models.CharField(max_length=75, blank=True)
+    skype = models.CharField(max_length=75, blank=True)
+    yahoo = models.EmailField(max_length=75, blank=True)
+
+#TODO: implement the 'staff_photo = models.ImageField()'
+    
 
 #TODO: implement an algorithm to return player's level based on xp
     def level():
@@ -18,11 +37,18 @@ class Player(models.Model):
     def __unicode__(self):
         return self.uwnetid
 
-class PlayerRevew(models.Model):
+class PlayerReview(models.Model):
     pass
 
 class PlayerWageHistory(models.Model):
     pass
 
 class Skills(models.Model):
+#See whiteboard captures for ideas
+    pass
+
+class Position(models.Model):
+#Attributes will be: Team, Title, Rank
+#Possible sub-options: Commentor, Consultant, Senior Consultant, Lead, Senior Lead, Developer, Instructor, Rookie, Manager, PC Lead, Mac Lead, Network Lead
+#Teams are: iTeam, Web Team, DS Team, Catalyst, Help Desk
     pass
