008f7342da9701f6752eae3dea46f2cd2713e7da
Jacob <jacob.morris@gmail.com>
2010-03-18 15:25:50 -0700

    Added views, templates, and urls for Chronos, to simply view a list of shifts.

--- chronos/views.py
+++ chronos/views.py
@@ -1,1 +1,6 @@
-# Create your views here.
+from django.shortcuts import render_to_response, get_object_or_404
+from labgeeksrpg.chronos.models import Shift 
+
+def report(request):
+    shifts = Shift.objects.all()
+    return render_to_response('report.html', locals())
