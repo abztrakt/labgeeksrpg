789e250aec77bf9089c8b6e3b7a99f98656a5950
Jacob <jacob.morris@gmail.com>
2010-03-24 14:26:20 -0700

    Added the time model and view, to be used as the "sign in sheet" for Chronos

--- chronos/views.py
+++ chronos/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render_to_response, get_object_or_404
 from labgeeksrpg.chronos.models import Shift 
+from labgeeksrpg.player.models import Player
 
 def report(request):
     shifts = Shift.objects.all()
     return render_to_response('report.html', locals())
+
+def time(request):
+    players = Player.objects.all()
+    return render_to_response('time.html', locals())
