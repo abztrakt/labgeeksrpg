4d3dfc3b93199481d4b8082b11a3cbb03b13e6a4
craig <craig@ubuntu.ubuntu-domain>
2010-05-12 17:17:13 -0700

    Added comments.

--- chronos/models.py
+++ chronos/models.py
@@ -2,6 +2,8 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class Location(models.Model):
+    """ The location of the punchclock, as well as the location staff are signing in for. This lets us use multiple computers on site as punchclocks.
+    """
     name = models.CharField(max_length=256)
     active_users = models.ManyToManyField(User, blank=True, null=True)
 
@@ -9,6 +11,8 @@ class Location(models.Model):
         return self.name
 
 class Punchclock(models.Model):
+    """ The computer we'll check ip address against to make sure staff are allowed to sign in from here.
+    """
     name = models.CharField(max_length=256)
     location = models.ForeignKey(Location)
     ip_address = models.IPAddressField()
@@ -17,6 +21,8 @@ class Punchclock(models.Model):
         return self.name
 
 class Shift(models.Model):
+    """ The shift the user is signing into or out of. A historical record of a work session.
+    """
     person = models.ForeignKey(User)
     intime = models.DateTimeField()
     outtime = models.DateTimeField(blank=True, null=True)
