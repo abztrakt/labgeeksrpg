1d04b3b7ec053c36db332659283d8780858dde73
craig <craig@ubuntu.ubuntu-domain>
2010-05-27 09:38:24 -0700

    Set up the people list view which currently shows all users in the system.

--- people/views.py
+++ people/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render_to_response
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
 
 def list_all(request):
     """ List all users in the system.
     """
-    return render_to_response('list.html')
+    users = User.objects.all()
+    return render_to_response('list.html', locals())
 
 @login_required
 def view_profile(request, name):
