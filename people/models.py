d3c3810d89e1f93ebddc6b8b2878f564c0afda17
craig <craig@ubuntu.ubuntu-domain>
2010-05-26 17:47:25 -0700

    Added docstring describing the uwer profile.

--- people/models.py
+++ people/models.py
@@ -2,6 +2,8 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class UserProfile(models.Model):
+    """ Defines additional things we should know about users.
+    """
     user = models.ForeignKey(User, unique=True)
     hp = models.IntegerField()
     xp = models.IntegerField()
