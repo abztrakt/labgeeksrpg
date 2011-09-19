55bbb286bb65c7e52e5eef7baa78dc9ce2984bd9
Craig M. Stimmel <cstimmel@uw.edu>
2011-09-19 14:32:58 -0700

    Disabled creation of users everytime one manages to log in via pubcookie - users will need to be in the db first to log in for now. This is a quick fix to the fact that any UW NetID owner could see staff shifts.

---
+++ auth.py
@@ -0,0 +1,16 @@
+from django.contrib.auth.models import User
+from django.contrib.auth.backends import RemoteUserBackend
+
+class UWRemoteUserBackend(RemoteUserBackend):
+    """ 
+    Checks for the $REMOTE_USER var and if it exists, trusts the user was authenticated successfully. If the user is successfully authenticated, but doesn't exist it will be created and added to the "Everyone" group, to get some default permissions.
+    """
+    RemoteUserBackend.create_unknown_user = False
+
+    #def configure_user(self, user):
+    #    """ 
+    #    Gives the user some default permissions.
+    #    """
+    #    everyone = Group.objects.get_or_create('Everyone')
+    #    user.groups.add(everyone)
+    #    return user
