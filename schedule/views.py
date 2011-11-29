6a83482b85ff9efdbcd5006c82995a10a20fc104
Michael Nguyen <mnnguyen@uw.edu>
2011-11-29 14:31:42 -0800

    Major changes to the create schedule view. Uses javascript to pre-load some of the data and allow users to interact with the schedule itself.

--- schedule/views.py
+++ schedule/views.py
@@ -113,26 +113,17 @@ def view_shifts(request):
 
 def view_timeperiods(request):
     timeperiod_stats = []
-    timeperiod_users = []
     timeperiods = TimePeriod.objects.all().order_by('start_date')
 
     for timeperiod in timeperiods:
         people = UserProfile.objects.filter(working_periods__name=timeperiod.name)
         data = {
             'timeperiod': timeperiod.name,
-            'start_date': timeperiod.start_date,
-            'end_date': timeperiod.end_date,
             'count': people.count(),
-            'people': people,
             'slug': timeperiod.slug
             }
 
-        users = {
-            'timeperiod': timeperiod.name,
-            'people': people
-        }
         timeperiod_stats.append(data)
-        timeperiod_users.append(users)
     if not timeperiods:
         message = 'Nobody available for timeperiods or nobody filled out preferences'
      
@@ -156,25 +147,52 @@ def view_timeperiod_data(request):
 
     return HttpResponse(result)
 
-def create_default_daily_schedule(request):
+def view_people(request):
+    #TODO change it so that only active people are displayed.
+    people_list = User.objects.all()
+    people = [str(c.username) for c in people_list]
+    result = json.dumps({
+        'people': people
+        })
+
+    return HttpResponse(result)
+
+
+def create_default_schedule(request):
     if request.method == 'POST':
         form = CreateDailyScheduleForm(request.POST)
         if form.is_valid():
-            day = form.cleaned_data['day']
-            day_defaults = DefaultShift.objects.filter(day=day).order_by('in_time')
 
-            if not day_defaults:
-                message = 'No default shifts created for %s' % (day)
+            # Grab the data from the form
+            timeperiod = TimePeriod.objects.get(name=form.cleaned_data['timeperiods'])
+            location = Location.objects.get(name=form.cleaned_data['location'])
+
+            # TODO add timeperiod with a default shift instead of looking up start date / end date.
+            start_date = timeperiod.start_date
+            end_date = timeperiod.end_date
+            
+            x_axis = ['Monday','Tuesday','Wednesday','Thursday','Friday','Saturday','Sunday']
+            y_axis = []
+
+            # y_axis - The time scale
+            counter = datetime(1,1,1,7,0)
+            
+            while counter.hour != 1:
+                y_axis.append(counter.time().strftime('%I:%M %p').lower())
+                counter += timedelta(minutes=30)
+            
+            shifts = []
+            for day in x_axis:
+                data = {'day': day,'shifts':y_axis,'location':location}
+                shifts.append(data)
+
+            schedule_class = "visible"
+            #import pdb; pdb.set_trace()
     else:
+        schedule_class ="hidden"
         form = CreateDailyScheduleForm()
     
     return render_to_response('create_schedule.html', locals(), context_instance=RequestContext(request))
 
 def view_preferences(request,form):
     pass
-
-
-def edit_preferences(request):
-    pass
-
-
