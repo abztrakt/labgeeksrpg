e8cf049df48852c40ef93dd33572dd4b1c7f7e8a
Aba <azri92@gmail.com>
2012-08-09 16:11:41 -0700

    search stuff

--- settings.py
+++ settings.py
@@ -156,6 +156,7 @@ INSTALLED_APPS = (
 HAYSTACK_SITECONF = 'labgeeksrpg.search_sites'
 HAYSTACK_SEARCH_ENGINE = 'whoosh'
 HAYSTACK_WHOOSH_PATH = 'labgeeksrpg/whoosh_index'
+HAYSTACK_INCLUDE_SPELLING = True
 
 LOGIN_URL = '/login/'
 SESSION_EXPIRE_AT_BROWSER_CLOSE = True
