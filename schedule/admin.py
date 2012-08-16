e0e97b1d943520e9a791524b2e7c44b385e2589a
qnnwp <qnnwp@uw.edu>
2012-08-16 15:35:46 -0700

    Added BaseShifts to the admin

--- schedule/admin.py
+++ schedule/admin.py
@@ -3,6 +3,7 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from django.contrib.auth.models import User
 
+admin.site.register(BaseShift)
 admin.site.register(ShiftType)
 admin.site.register(DefaultShift)
 admin.site.register(WorkShift)
