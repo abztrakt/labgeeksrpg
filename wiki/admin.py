e589848f57e419dd992ea4473ba1bb850ceb69ab
Aba <azri92@gmail.com>
2012-08-16 16:40:57 -0700

    updates for slug in admin, models, views & urls. migrations and an empty form.

---
+++ wiki/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from labgeeksrpg.wiki.models import Page
+
+class PageAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('name',)}
+
+admin.site.register(Page, PageAdmin)
