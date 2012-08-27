cdd5613b8f6f25d68a610aa3898ecf2ec6e70d51
Aba <azri92@gmail.com>
2012-08-26 19:38:53 -0700

    search feature still in progress

--- knowledgebase/search_indexes.py
+++ knowledgebase/search_indexes.py
@@ -1,18 +1,29 @@
 import datetime
 from haystack.indexes import *
 from haystack import site
-from knowledgebase.models import Answer
+from knowledgebase.models import Question, Answer
 
-class AnswerIndex(RealTimeSearchIndex):
+class QuestionIndex(RealTimeSearchIndex):
     text = CharField(document=True, use_template=True)
-    question = CharField(model_attr='question', null=True)
     user = CharField(model_attr='user', null=True)
     date = DateTimeField(model_attr='date')
 
-    content_auto = EdgeNgramField(model_attr='content')
+    content_auto = EdgeNgramField(model_attr='more_info')
+
+    def index_queryset(self):
+        """Used when the entire index for model is updated"""
+        return Question.objects.filter(date__lte=datetime.datetime.now())
+
+class AnswerIndex(RealTimeSearchIndex):
+    text = CharField(document=True, use_template=True)
+    user = CharField(model_attr='user', null=True)
+    date = DateTimeField(model_attr='date')
+
+    content_auto = EdgeNgramField(model_attr='answer')
 
     def index_queryset(self):
         """Used when the entire index for model is updated."""
         return Answer.objects.filter(date__lte=datetime.datetime.now())
 
+site.register(Question, QuestionIndex)
 site.register(Answer, AnswerIndex)
