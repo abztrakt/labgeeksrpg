b028a7b78877b08efb1aa003bd33469e5c85b524
Jon Swanson <jswan2@uw.edu>
2012-09-18 11:33:24 -0700

    Fixed template syntax error

--- schedule/urls.py
+++ schedule/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls.defaults import *
 
 urlpatterns = patterns('labgeeksrpg.schedule.views',
                        url(r'^view_preferences/$', 'view_preferences', name="Schedule-View_Prefs"),
-                       #  url(r'^create/$', 'create_default_schedule', name="Schedule-Create_Schedule"),
+                       url(r'^create/$', 'create_default_schedule', name="Schedule-Create_Schedule"),
                        url(r'^shifts/$', 'view_shifts', name="Schedule-View_Shifts"),
                        url(r'^available/$', 'view_available_shifts', name="Schedule-View_Avail"),
                        url(r'^timeperiods/$', 'view_timeperiods', name="Schedule-View_Timeperiods"),
