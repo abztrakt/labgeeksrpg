8ef76fdfa97e37414c6c01d7c343d95aaacf9a07
Craig M. Stimmel <cstimmel@uw.edu>
2012-12-18 10:37:08 -0800

    Make the shift list filterable for investingating weirdness.

--- chronos/admin.py
+++ chronos/admin.py
@@ -1,6 +1,11 @@
 from labgeeksrpg.chronos.models import *
 from django.contrib import admin
 
-admin.site.register(Shift)
+
+class ShiftAdmin(admin.ModelAdmin):
+    list_filter = ('person',)
+    search_fields = ['person', 'shiftnote']
+admin.site.register(Shift, ShiftAdmin)
+
 admin.site.register(Location)
 admin.site.register(Punchclock)
